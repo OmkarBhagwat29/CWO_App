289693e37e48067958c38fd847abfd9ec7d02d37
Omkar-Bhagwat <ombhagwat29@gmail.com>
2024-04-14 23:20:23 +0200

    inital Setup

---
+++ build/Build.cs
@@ -0,0 +1,14 @@
+using Nuke.Common.Git;
+using Nuke.Common.ProjectModel;
+
+sealed partial class Build : NukeBuild
+{
+    string[] Configurations;
+    Project[] Bundles;
+    Dictionary<Project, Project> InstallersMap;
+
+    [GitRepository] readonly GitRepository GitRepository;
+    [Solution(GenerateProjects = true)] Solution Solution;
+
+    public static int Main() => Execute<Build>(x => x.Compile);
+}
